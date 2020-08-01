10dbc2ec3b34f5d14a0ad7d5cb82e34eee4f9fd6
SeungwanJeon <37177558+SeungwanJeon@users.noreply.github.com>
2020-08-01 16:22:11 +0900

    add scanf and sprintf

--- basic_examples/HelloWorld/main.cpp
+++ basic_examples/HelloWorld/main.cpp
@@ -1,5 +1,13 @@
 #include<stdio.h>
 
-void main() {
+int main() {
 	printf("hellow world\n");
+
+	char buffer[50];
+	int n, a, b;
+	scanf_s("%d %d", &a, &b);
+	n = sprintf_s(buffer, "%d plus %d is %d", a, b, a + b);
+	printf("[%s] is a string %d chars long \n", buffer, n);
+
+	return 0;
 }
